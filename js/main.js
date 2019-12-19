5487b567fc0122f3aec0b25a45a66b795915f557
Arindam Lahiri <arindamlahiri17@gmail.com>
2019-12-19 22:05:37 +0530

    domains

--- js/main.js
+++ js/main.js
@@ -11,3 +11,32 @@ $(function() {
     });
   
 });
+
+const sections = document.querySelectorAll("section");
+
+const options = {
+    root: null,
+    threshold: 0.4,
+    rootMargin: "0px"
+}
+
+const observer = new IntersectionObserver((entries,observer) => {
+    entries.forEach((entry) => {
+        if(entry.target.id === "about-us")
+            if(entry.isIntersecting)
+                document.getElementById("li-about-us").style = "border-bottom: 3px solid #EA4335;font-weight: 700;"
+            else
+                document.getElementById("li-about-us").style = "border-bottom:none;font-weight: 400;"
+        
+        if(entry.target.id === "domains")
+            if(entry.isIntersecting)
+                document.getElementById("li-domains").style = "border-bottom: 3px solid #EA4335;font-weight: 700;"
+            else
+                document.getElementById("li-domains").style = "border-bottom:none;font-weight: 400;"
+
+    });
+}, options);
+
+sections.forEach((section) => {
+    observer.observe(section);
+});
